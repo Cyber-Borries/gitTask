46f613d08c8e75ac725cd45b51bcc7117569cef7
Cyber-Borries <adriaan.bornman1@gmail.com>
2022-07-04 14:12:54 +0200

    added HTML and CSS file

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,9 @@
-console.log("Git is awesome");
+function displayInput() {
+  const inputBox = document.getElementById("inputEl").value;
+  const div = document.getElementById("userDiv");
+  // let userInput = "";
+  // inputBox.value = userInput;
+  // div.innerHTML = userInput;
+  div.innerHTML = inputBox;
+  div.style.backgroundColor = "darkblue";
+}
